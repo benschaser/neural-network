ffd11b25d3ae2cbcef6b5e2fb6473b10c713c381
benschaser <benschaser@gmail.com>
2025-07-19 19:21:51 -0500

    implemented neural network propogations

--- src/neural_network.cpp
+++ src/neural_network.cpp
@@ -1,9 +1,11 @@
 #include "neural_network.h"
 
-NeuralNetwork::NeuralNetwork(vec<int> topology, double learningRate)
+NeuralNetwork::NeuralNetwork(std::string dataset_label, vec<int> topology, double learningRate)
 {
+    this->dataset_label = dataset_label;
+    this->topology = topology;
     this->learningRate = learningRate;
-    for (int i = 0; i < topology.size(); ++i)
+    for (ulong i = 0; i < topology.size(); ++i)
     {
         // init neuron layers
         if (i == topology.size() - 1)
@@ -39,3 +41,63 @@ NeuralNetwork::NeuralNetwork(vec<int> topology, double learningRate)
         }
     }
 }
+
+void NeuralNetwork::propogateForward(RowVector& input) {
+    neuronLayers.front()->block(0, 0, 1, neuronLayers.front()->size() - 1) = input;
+    for (ulong i = 1; i < topology.size(); ++i) {
+        (*neuronLayers[i]) = (*neuronLayers[i - 1]) * (*weights[i - 1]);
+        neuronLayers[i]->block(0, 0, 1, topology[i]).unaryExpr(activationFunction);
+    }
+}
+
+void NeuralNetwork::propogateBackward(RowVector& output) {
+    calcErrors(output);
+    updateWeights();
+}
+
+void NeuralNetwork::calcErrors(RowVector& output) {
+    (*deltas.back()) = output - (*neuronLayers.back());
+
+    for (ulong i = 0; i < topology.size() - 1; ++i) {
+        (*deltas[i]) = (*deltas[i + 1]) * (weights[i]->transpose());
+    }
+}
+
+void NeuralNetwork::updateWeights() {
+    for (ulong i = 0; i < topology.size() - 1; ++i) {
+        if (i != topology.size() - 2) {
+            for (ulong c = 0; c < weights[i]->cols() - 1; ++c) {
+                for (ulong r = 0; r < weights[i]->rows(); r++) {
+                    weights[i]->coeffRef(r, c) += learningRate * deltas[i + 1]->coeffRef(c) * activationFunctionDerivative(cacheLayers[i + 1]->coeffRef(c)) * neuronLayers[i]->coeffRef(r);
+                }
+            }
+        }
+        else {
+            for (ulong c = 0; c < weights[i]->cols(); ++c) {
+                for (ulong r = 0; r < weights[i]->rows(); r++) {
+                    weights[i]->coeffRef(r, c) += learningRate * deltas[i + 1]->coeffRef(c) * activationFunctionDerivative(cacheLayers[i + 1]->coeffRef(c)) * neuronLayers[i]->coeffRef(r);
+                }
+            }
+        }
+    }
+}
+
+void NeuralNetwork::train(vec<RowVector*> input, vec<RowVector*> output) {
+    for (ulong i = 0; i < input.size(); ++i) {
+        std::cout << get_env_label() << "Training on input: " << *input[i] << '/' << input.size() << '\n';
+        propogateForward(*input[i]);
+        std::cout << get_env_label() << "\33[33mExpected value: " << *output[i] << "\33[0m\n";
+        if (*output[i] == *neuronLayers.back()) {
+            std::cout << get_env_label() << "\33[32mOutput value: " << *neuronLayers.back() << "\33[0m\n";
+        }
+        else {
+            std::cout << get_env_label() << "\33[31mOutput value: " << *neuronLayers.back() << "\33[0m\n";
+        }
+        propogateBackward(*output[i]);
+        std::cout << get_env_label() << "MSE: " << std::sqrt((*deltas.back()).dot((*deltas.back())) / deltas.back()->size()) << '\n';
+    }
+ }
+ 
+ std::string NeuralNetwork::get_env_label() {
+    return "[" + dataset_label + "] ";
+ }
--- src/neural_network.h
+++ src/neural_network.h
@@ -7,21 +7,34 @@ using ColVector = Eigen::VectorXf;
 using Matrix = Eigen::MatrixXf;
 template <typename T>
 using vec = std::vector<T>;
+using ulong = unsigned long;
+
+const double activationFunction(double x) {
+    return tanhf(x);
+} 
+const double activationFunctionDerivative(double x) {
+    return 1.0 - tanhf(x) * tanhf(x);
+}
 
 class NeuralNetwork
 {
 public:
-    NeuralNetwork(vec<int> topology, double learningRate = 0.005);
+    NeuralNetwork(std::string dataset_label, vec<int> topology, double learningRate = 0.005);
 
     void propogateForward(RowVector &input);
     void propogateBackward(RowVector &output);
     void calcErrors(RowVector &output);
     void updateWeights();
-    void train(vec<RowVector *> data);
+    void train(vec<RowVector *> input, vec<RowVector*> output);
+    std::string get_env_label();
 
+    std::string dataset_label = "default";
+    vec<int> topology;
     vec<RowVector *> neuronLayers;
     vec<RowVector *> cacheLayers;
     vec<RowVector *> deltas;
     vec<Matrix *> weights;
     double learningRate;
+    std::function<double(double)> activationFunction = activationFunction;
+    std::function<double(double)> activationFunctionDerivative = activationFunctionDerivative;
 };
