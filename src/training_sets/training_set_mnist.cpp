75f469623d4b5385e2da3e236408048a843411b8
benschaser <benschaser@gmail.com>
2025-08-04 14:50:06 -0500

    started mnist csv load functionality

---
+++ src/training_sets/training_set_mnist.cpp
@@ -0,0 +1,67 @@
+#include "training_set_mnist.h"
+#include "aliases.h"
+#include <fstream>
+#include <sstream>
+/**
+ * @brief Gets the total line count of the file.
+ *
+ * This function takes in a filepath and returns the line count.
+ *
+ * @param filepath The absolute filepath represented as a string.
+ * @return Line count. If -1 is returned, the file could not be found or opened.
+ */
+int count_lines(const std::string filepath)
+{
+    std::ifstream file(filepath);
+    if (!file.is_open())
+        return -1;
+
+    int count = 0;
+    std::string line;
+    while (std::getline(file, line))
+        ++count;
+
+    return count;
+}
+
+bool TrainingSetMNIST::load_data(std::string arg)
+{
+    std::cout << "Loading MNIST data from: " << arg << std::endl;
+    int line_count = count_lines(arg);
+    if (line_count == -1)
+        return false;
+
+    std::ifstream file(arg);
+    if (!file.is_open())
+        return false;
+
+    std::string line;
+    int count = 0;
+
+    while (std::getline(file, line))
+    {
+        if (count >= 100000)
+            break; // shouldn't go over 100,000 samples
+
+        std::stringstream ss(line);
+        std::string cell;
+
+        // read label
+        std::getline(ss, cell, ',');
+        int label = std::stoi(cell);
+        // TO DO: add label to expected output;
+
+        // read pixels (784 count)
+        RowVector row(784);
+        for (int i = 0; i < 784; ++i)
+        {
+            if (!std::getline(ss, cell, ','))
+                throw std::runtime_error("Malformed CSV line: " + line);
+
+            row(i) = std::stoi(cell) / 255.0; // normalized
+        }
+        input.push_back(new RowVector(row));
+    }
+
+    return true;
+}
--- src/training_sets/training_set_mnist.h
+++ src/training_sets/training_set_mnist.h
@@ -2,15 +2,11 @@
 #include "aliases.h"
 #include "dataset.h"
 
-class TrainingSetMNIST : public Dataset {
+class TrainingSetMNIST : public Dataset
+{
 public:
-    TrainingSetMNIST(std::string label)
-        : Dataset(label) {}
+    TrainingSetMNIST()
+        : Dataset("mnist") {}
 
-    bool load_data(std::string arg) override {
-        std::cout << "Loading MNIST data from: " << arg << std::endl;
-
-        
-        return true;
-    }
+    bool load_data(std::string arg) override;
 };
